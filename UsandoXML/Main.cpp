50c9b82eecfae0669052a2292a29bea3a601e5a3
Jeremy Fonseca <alx.17@outlook.com>
2022-09-14 23:18:38 -0600

    feat: Add sub-projects for files

---
+++ UsandoXML/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int32_t main()
+{
+  std::cout << "Proyecto usando XML" << std::endl;
+  return EXIT_SUCCESS;
+}
---
+++ UsandoYML/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int32_t main()
+{
+  std::cout << "Proyecto usando YML" << std::endl;
+  return EXIT_SUCCESS;
+}
