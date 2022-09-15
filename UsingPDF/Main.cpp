c16858b9a07867ca42524f76d3540fc4187c822b
Jeremy Fonseca <alx.17@outlook.com>
2022-09-15 01:23:48 -0600

    feat: Add basics for PDF handling

--- UsingPDF/Main.cpp
+++ UsingPDF/Main.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <string>
 #include <hpdf.h>
+#include <string.h>
 
 struct PdfUserData
 {
@@ -11,50 +12,73 @@ struct PdfUserData
 };
 class PdfExeption : public std::exception
 {
-  public:
+public:
+  PdfExeption(const HPDF_UINT error_no, const HPDF_UINT detail_no, PdfUserData *const user_data = nullptr)
+      : ErrorNo{error_no}, DetailNo{detail_no}, UserData{user_data}
+  {
+  }
 
-    PdfExeption(const HPDF_UINT error_no, const HPDF_UINT detail_no, PdfUserData* const user_data = nullptr)
-      : ErrorNo{ error_no }
-      , DetailNo{ detail_no }
-      , UserData{ user_data }
-      {
-      }
+  virtual const char *what() const noexcept
+  {
+    sprintf(this->Buffer, "Error number: %u, Detail number: %u", this->ErrorNo, this->DetailNo);
 
-    virtual const char* what() const noexcept
+    if (UserData != nullptr)
     {
-      std::ostringstream out;
-      out << "Error number: " << std::hex << this->ErrorNo
-          << ", Detail Number: " << std::hex << this->DetailNo;
-
-      if (UserData != nullptr)
-      {
-        out << ", Error Message: " << this->UserData->Message;
-      }
-
-      return out.str().c_str();
+      auto prev_text = strdup(this->Buffer);
+      sprintf(this->Buffer, "%s, Error message: %s.", prev_text, this->UserData->Message.c_str());
+      free(prev_text);
     }
+    return this->Buffer;
+  }
 
-  private:
-    HPDF_UINT ErrorNo;
-    HPDF_UINT DetailNo;
-    PdfUserData* UserData;
+private:
+  HPDF_UINT ErrorNo;
+  HPDF_UINT DetailNo;
+  PdfUserData *UserData;
+  mutable char Buffer[2048]; // Se puede modificar aunque el método sea const
 };
 
-[[noreturn]] void HaruPdfErrorHandler(HPDF_STATUS error_no, HPDF_STATUS detail_no, void* user_data)
+[[noreturn]] void HaruPdfErrorHandler(HPDF_STATUS error_no, HPDF_STATUS detail_no, void *user_data)
 {
   throw PdfExeption(static_cast<HPDF_UINT>(error_no),
                     static_cast<HPDF_UINT>(detail_no),
-                    static_cast<PdfUserData*>(user_data));
+                    static_cast<PdfUserData *>(user_data));
 }
 
 int32_t main()
 {
+  PdfUserData pdf_user_data{"Creación del PDF "};
+  HPDF_Doc pdf = HPDF_NewEx(HaruPdfErrorHandler, nullptr, nullptr, 0, &pdf_user_data);
+
   try
   {
-    HPDF_Doc pdf = HPDF_New(HaruPdfErrorHandler, nullptr);
+    // Crear fuente para el pdf
+    pdf_user_data = PdfUserData{"Creando fuente del PDF..."};
+    HPDF_Font pdf_font = HPDF_GetFont(pdf, "Helvetica", NULL);
+
+    pdf_user_data = PdfUserData{"Agregando una página..."};
+    HPDF_Page page1 = HPDF_AddPage(pdf);
+    HPDF_Page_SetWidth(page1, 400);
+    HPDF_Page_SetHeight(page1, 500);
+
+    // Y-axis is inverted
+
+    pdf_user_data = PdfUserData{"Iniciando texto..."};
+    HPDF_Page_BeginText(page1);
+
+    // Write to the PDF
+    HPDF_Page_SetFontAndSize(page1, pdf_font, 20);
+    HPDF_Page_MoveTextPos(page1, 20, 450);
+    HPDF_Page_ShowText(page1, "C++ Document PDF");
+    HPDF_Page_EndText(page1);
+
+    pdf_user_data = PdfUserData{"Guardando pdf..."};
+    HPDF_SaveToFile(pdf, "Test.pdf");
+    HPDF_Free(pdf);
   }
-  catch(const PdfExeption& e)
+  catch (const PdfExeption &e)
   {
+    HPDF_Free(pdf);
     std::cerr << e.what() << '\n';
   }
 }
